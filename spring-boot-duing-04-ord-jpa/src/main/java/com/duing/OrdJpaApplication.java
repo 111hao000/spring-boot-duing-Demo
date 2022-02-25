f5da7c5ffe407ed359036ff5bc6bd3e1c7517266
111hao000 <2458628180@qq.com>
2022-02-25 13:54:10 +0800

    更新了template

---
+++ spring-boot-duing-04-ord-jpa/src/main/java/com/duing/OrdJpaApplication.java
@@ -0,0 +1,13 @@
+package com.duing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdJpaApplication.class, args);
+    }
+
+}
