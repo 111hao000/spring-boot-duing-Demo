fd83fbea089ec7723113e08afb72ad702f9d7109
111hao000 <2458628180@qq.com>
2022-03-03 22:29:44 +0800

    springboot-duing-06-error-exception

--- spring-boot-duing-06-erroe-exception/src/main/java/com/duing/handler/MyExceptionHandler.java
+++ spring-boot-duing-06-erroe-exception/src/main/java/com/duing/handler/MyExceptionHandler.java
@@ -4,9 +4,12 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 全局异常配置
+ */
 @ControllerAdvice
 public class MyExceptionHandler {
-
+    //全局异常配置
     @ExceptionHandler(Exception.class)
     public ModelAndView handler(Exception e){
         ModelAndView mv = new ModelAndView();
